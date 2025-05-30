ab35beb9a9883bd1a4817c2f37e9a92a7dbffc83
De-Graft <108901826+EDe-Graft@users.noreply.github.com>
2025-05-30 03:43:03 -0400

    Delete frontend directory

--- frontend/src/pages/NoPageFound.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NoPageFound ()  {
-    return(
-        <div className="container flex flex-col items-center text-center max-w-3xl mx-auto mx-auto px-4 py-8 sm:min-h-[60vh]">
-            <p>Page does not exist</p>
-        </div>
-    )
-}
