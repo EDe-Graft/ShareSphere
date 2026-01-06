605366f39aab5cb106ce35da4a50cf6e8b93bce3
De-Graft <108901826+EDe-Graft@users.noreply.github.com>
2026-01-06 16:25:33 -0500

    updated authcontext backend env variable call

--- client/src/components/context/AuthContext.jsx
+++ client/src/components/context/AuthContext.jsx
@@ -2,7 +2,7 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BACKEND_URL = import.meta.env.NODE_ENV === 'production' ? import.meta.env.VITE_BACKEND_URL : 'http://localhost:3000';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 // Always send cookies
 const axiosConfig = {
